c46abb61dc13c7eafd7ac0fb8027eefe4dd42aca
Lars-Erik <lars-erik@aabech.no>
2025-10-18 22:06:07 +0200

    tests: POC for all three subjunctions with behavior and shared state

--- FluentNetBDD.Tests/Dsl/Building_Feature_Subjunctions.cs
+++ FluentNetBDD.Tests/Dsl/Building_Feature_Subjunctions.cs
@@ -7,6 +7,12 @@ using FluentNetBDD.Dsl;
 
 public class Building_Feature_Subjunctions
 {
+    public class UserGreetingFeature
+    {
+        public const string UserName = nameof(UserName);
+        public const string UserGreeting = nameof(UserGreeting);
+    }
+
     public interface IUserWithName
     {
         string Name { get; }
@@ -15,34 +21,115 @@ public class Building_Feature_Subjunctions
         void WithName(string name);
     }
 
+    public interface IUserGreetingAction
+    {
+        void IsGreeted();
+    }
+
+    public interface IUserGreetingVerification
+    {
+        void Hears(string greeting);
+    }
+
     public interface IGivenUserWithName
     {
         IUserWithName User { get; }
     }
 
+    public interface IWhenUserGreeting
+    {
+        IUserGreetingAction User { get; }
+    }
+
+    public interface IThenUserGreeting
+    {
+        IUserGreetingVerification User { get; }
+    }
+
     class UserWithName : IUserWithName
     {
+        private readonly DslState state;
         public string Name { get; private set; } = string.Empty;
         public void WithName(string name)
         {
             Name = name;
+            state.Set(UserGreetingFeature.UserName, name);
+        }
+
+        public UserWithName(DslState state)
+        {
+            this.state = state;
         }
     }
 
-    private IServiceProvider provider;
+    class UserGreetingAction : IUserGreetingAction
+    {
+        private readonly DslState state;
 
-    [SetUp]
+        public UserGreetingAction(DslState state)
+        {
+            this.state = state;
+        }
+
+        public void IsGreeted()
+        {
+            var userName = state.Get(UserGreetingFeature.UserName);
+            state.Set(UserGreetingFeature.UserGreeting, $"Hi, {userName}!");
+        }
+    }
+
+    class UserGreetingVerification : IUserGreetingVerification
+    {
+        private readonly DslState state;
+        public UserGreetingVerification(DslState state)
+        {
+            this.state = state;
+        }
+        public void Hears(string greeting)
+        {
+            var actualGreeting = state.Get(UserGreetingFeature.UserGreeting);
+            Assert.That(actualGreeting, Is.EqualTo(state.Get(UserGreetingFeature.UserGreeting)));
+        }
+    }
+
+    private IServiceProvider mainProvider;
+    private IServiceProvider provider;
+    private object untypedDsl;
+
+    private static readonly Type DslType = typeof(Dsl<,,>);
+
+    [OneTimeSetUp]
     public void SetUpServices()
     {
         var services = new ServiceCollection();
-        services.AddTransient<IUserWithName, UserWithName>();
-        provider = services.BuildServiceProvider();
+
+        services.AddScoped<IUserWithName, UserWithName>();
+        services.AddScoped<IUserGreetingAction, UserGreetingAction>();
+        services.AddScoped<IUserGreetingVerification, UserGreetingVerification>();
+
+        services.AddScoped<DslState>();
+
+        mainProvider = services.BuildServiceProvider();
+    }
+
+    [SetUp]
+    public void SetUpProvider()
+    {
+        provider = mainProvider.CreateScope().ServiceProvider;
+
+        untypedDsl = CreateDsl(provider);
     }
 
     [TearDown]
-    public void DisposeServices()
+    public void DisposeProvider()
     {
         (provider as IDisposable)?.Dispose();
+    }
+
+    [OneTimeTearDown]
+    public void DisposeMainProvider()
+    {
+        (mainProvider as IDisposable)?.Dispose();
     }
 
     [Test]
@@ -54,11 +141,47 @@ public class Building_Feature_Subjunctions
         Assert.That(dynamicGiven.User.Name, Is.EqualTo("Neo"));
     }
 
-    [Test]
-    public void Constructs_Dsl_With_Specified_Given_Proxy()
+    [DslTestCase<IGivenUserWithName, IWhen, IThen>]
+    public void Constructs_Dsl_With_Specified_Given_Proxy(params Type[] args)
     {
-        var dsl = new Dsl<IGivenUserWithName, IWhen, IThen>(provider);
+        var dsl = (Dsl<IGivenUserWithName, IWhen, IThen>)untypedDsl;
+
         dsl.Given.User.WithName("Trinity");
         Assert.That(dsl.Given.User.Name, Is.EqualTo("Trinity"));
     }
+
+    [DslTestCase<IGivenUserWithName, IWhenUserGreeting, IThenUserGreeting>]
+    public void Constructs_Dsl_With_Proxies_For_All_Subjunctions(params Type[] args)
+    {
+        var dsl = (Dsl<IGivenUserWithName, IWhenUserGreeting, IThenUserGreeting>)untypedDsl;
+
+        dsl.Given.User.WithName("Morpheus");
+        dsl.When.User.IsGreeted();
+        dsl.Then.User.Hears("Hi, Morpheus!");
+    }
+
+    private static object CreateDsl(IServiceProvider provider)
+    {
+        var types = TestContext.CurrentContext.Test.Arguments.OfType<Type[]>().SelectMany(x => x).ToArray();
+        if (!types.Any())
+        {
+            types = TestContext.CurrentContext.Test.Arguments.OfType<Type>().ToArray();
+        }
+
+        if (!types.Any())
+        {
+            types = [typeof(IGiven), typeof(IWhen), typeof(IThen)];
+        }
+
+        if (types.Length == 3)
+        {
+            var genericDslType = DslType.MakeGenericType(types);
+            return Activator.CreateInstance(genericDslType, [provider])!;
+        }
+
+        throw new Exception("Use params Type[] args with only three types per test to build our Dsl");
+    }
 }
+
+public class DslTestCaseAttribute<TGiven, TWhen, TThen>()
+    : TestCaseAttribute(typeof(TGiven), typeof(TWhen), typeof(TThen));
--- FluentNetBDD/Dsl/Dsl.cs
+++ FluentNetBDD/Dsl/Dsl.cs
@@ -1,21 +1,42 @@
 ﻿using FluentNetBDD.Dsl.Builders;
 
-namespace FluentNetBDD.Dsl
-{
-    public class Dsl<TGiven, TWhen, TThen>
-        where TGiven : class
-        where TWhen : class
-        where TThen : class
-    {
-        public TGiven Given { get; private set; }
-        public TWhen When { get; private set; }
-        public TThen Then { get; private set; }
+namespace FluentNetBDD.Dsl;
 
-        public Dsl(IServiceProvider provider)
+public class Dsl<TGiven, TWhen, TThen>
+    where TGiven : class
+    where TWhen : class
+    where TThen : class
+{
+    public TGiven Given { get; private set; }
+    public TWhen When { get; private set; }
+    public TThen Then { get; private set; }
+
+    public Dsl(IServiceProvider provider)
+    {
+        Given = SubjunctionBuilder.Create<TGiven>(provider);
+        When = SubjunctionBuilder.Create<TWhen>(provider);
+        Then = SubjunctionBuilder.Create<TThen>(provider);
+    }
+}
+
+public class DslState
+{
+    private readonly Dictionary<string, object?> stateBag = new();
+
+    public object? Get(string key)
+    {
+        return stateBag.GetValueOrDefault(key);
+    }
+
+    public void Set(string key, object? value, bool overwrite = true)
+    {
+        if (!stateBag.ContainsKey(key))
         {
-            Given = SubjunctionBuilder.Create<TGiven>(provider);
-            When = SubjunctionBuilder.Create<TWhen>(provider);
-            Then = SubjunctionBuilder.Create<TThen>(provider);
+            stateBag.Add(key, value!);
+        }
+        else if (overwrite)
+        {
+            stateBag[key] = value!;
         }
     }
 }
